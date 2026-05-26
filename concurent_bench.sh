bc52694a7b33fac34608a557b008bce43a1eae38
Ubuntu <ubuntu@ip-172-31-45-13.us-east-2.compute.internal>
2026-05-26 11:19:31 +0000

    modfy script

--- concurent_bench.sh
+++ concurent_bench.sh
@@ -2,34 +2,13 @@
 
 N_INSTANCES="$1"
 
-for i in $(seq 0 $((N_INSTANCES - 1))); do
-    taskset -c "$i" \
-        python run_inference.py --framework=xgboost --n_instances="$N_INSTANCES" --instance_index=${i} &
+for fw in xgboost daal4py treelite onnx; do
+	for i in $(seq 0 $((N_INSTANCES - 1))); do
+	    taskset -c "$i" \
+        	python run_inference.py --framework=$fw --n_instances="$N_INSTANCES" --instance_index=${i} &
+	done
+
+	wait
+	python collect.py --framework=$fw --n_instances="$N_INSTANCES"
 done
 
-wait
-python collect.py --framework=xgboost --n_instances="$N_INSTANCES"
-
-for i in $(seq 0 $((N_INSTANCES - 1))); do
-    taskset -c "$i" \
-        python run_inference.py --framework=daal4py --n_instances="$N_INSTANCES" --instance_index=${i} &
-done
-
-wait
-python collect.py --framework=daal4py --n_instances="$N_INSTANCES"
-
-for i in $(seq 0 $((N_INSTANCES - 1))); do
-    taskset -c "$i" \
-        python run_inference.py --framework=treelite --n_instances="$N_INSTANCES" --instance_index=${i} &
-done
-
-wait
-python collect.py --framework=treelite --n_instances="$N_INSTANCES"
-
-for i in $(seq 0 $((N_INSTANCES - 1))); do
-    taskset -c "$i" \
-        python run_inference.py --framework=onnx --n_instances="$N_INSTANCES" --instance_index=${i} &
-done
-
-wait
-python collect.py --framework=onnx --n_instances="$N_INSTANCES"
